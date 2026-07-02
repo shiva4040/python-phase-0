53f5b375554b3389b25444545079879f20f15483
shiva4040 <shiva19503@gmail.com>
2026-07-02 10:50:30 +0530

    FizzBuzz is a classic beginner programming exercise

--- 2_july.py
+++ 2_july.py
@@ -1,5 +1,26 @@
-num = 2
-if num > 5 :
-    print("large number")
-else:
-    print("small number")
+# num = 2
+# if num > 5 :
+#     print("large number")
+# else:
+#     print("small number")
+
+#FizzBuzz is a classic beginner programming exercise
+# The rules
+
+# Print the numbers from 1 to 100, but:
+
+# If a number is divisible by 3, print "Fizz".
+# If a number is divisible by 5, print "Buzz".
+# If a number is divisible by both 3 and 5, print "FizzBuzz".
+# Otherwise, print the number itself.
+
+for i in range(1,17):
+    # print(i)
+    if(i%3 == 0 and i%5 ==0):
+        print("FizzBuzz") 
+    elif(i%3 == 0):
+        print("Fizz")
+    elif(i%5 == 0):
+        print("Buzz")
+    else:
+        print(i)
